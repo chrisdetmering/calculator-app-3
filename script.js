76cd7a192fa39f488984b50424feb065ec1d110e
Rheanna Gallego <gallego.rheanna@gmail.com>
2021-04-26 19:19:04 -0700

    Adding function to chain calculator functions

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
-let currentSelection = '0';
-let fullSelection = []
+let currentSelection = '';
+let currentOperator = '';
+let fullSelection = [];
+let total = 0;
+let firstNumber = 0;
+let secondNumber = 0;
 
-function updateDisplay(number){
-    currentSelection = number;
-    document.getElementById('current-selection').textContent = number;
 
+function selectNumber(number){
+    currentSelection += number;
     fullSelection.push(number);
-    document.getElementById('full-selection').textContent += number;
+
+    updateDisplay();
+}
+
+function updateDisplay(){
+    document.getElementById('current-selection').textContent = currentSelection;
+    document.getElementById('full-selection').textContent = '';
+    fullSelection.map((item) => document.getElementById('full-selection').textContent += item);
 }
 
 function clearDisplay(){
-    currentSelection = '0'
-    fullSelection = []
 
-    document.getElementById('current-selection').textContent = '0';
+    document.getElementById('current-selection').textContent = '0'
     document.getElementById('full-selection').textContent = '';
 }
 
 function addOperator(operator){
-    fullSelection.push(operator);
     document.getElementById('full-selection').textContent += operator;
+
+    if(currentOperator.length === 0){
+        currentOperator = operator;
+        firstNumber = currentSelection;
+        fullSelection.push(operator);
+        currentSelection = '';
+    } else {
+        switch(currentOperator){
+            case 'x':
+                total = parseFloat(firstNumber) * parseFloat(currentSelection);
+                break;
+            case '/':
+                total = parseFloat(firstNumber) / parseFloat(currentSelection);
+                break;
+            case '+':
+                total = parseFloat(firstNumber) + parseFloat(currentSelection);
+                break;
+            case '-':
+                total = parseFloat(firstNumber) - parseFloat(currentSelection);
+                break;
+        }
+
+        currentSelection = total;
+        firstNumber = total;
+        fullSelection.push(operator);
+        updateDisplay()
+
+        currentSelection = '';
+        currentOperator = operator;
+    }
 }
