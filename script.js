b412efb80045fbd11cac5035f3c2bbc68b0786af
froggy <chrisdetmering@gmail.com>
2021-04-28 13:14:39 -0700

    andy review compeleted

--- script.js
+++ script.js
@@ -1,143 +1,81 @@
-//Global variables
-let currentSelection = '';
-let currentOperator = '';
-let fullOperation = [];
-let total = 0;
-let firstNumber = 0;
+let firstOperand = '';
+let secondOperand = '';
+let operation = '';
 
-/***************************  Button Functions ****************************/
 document.querySelectorAll(".number").forEach(numberButton => { 
     numberButton.addEventListener("click", () => { 
-        const number = numberButton.textContent; 
-        setNumber(number);
+        setNumber(numberButton.textContent);
     })
 })
 
-
 function setNumber(number){
-    if(number === '.'){
-        if(fullOperation[fullOperation.length - 1] === '.') return
-        if(currentSelection.includes('.')) return
-    }
-    if(currentSelection.length === 10) return
-    if(currentSelection.length === 0 && number === 0) return
-
-    currentSelection += number;
-    fullOperation.push(number);
-
-    updateDisplay();
+  if (!operation) {
+      firstOperand += number; 
+      setDisplay(firstOperand);
+  } else { 
+      secondOperand += number; 
+      setDisplay(secondOperand);
+  }
 }
 
 document.querySelector(".clear")
 .addEventListener("click", () => { 
-    clearDisplay();
+    resetCalculator();
 })
-
-
-function clearDisplay(){
-    document.getElementById('current-selection').textContent = '0'
-    document.getElementById('full-selection').textContent = '';
-    resetValues();
-}
 
 document.querySelectorAll(".operator").forEach(operatorButton => { 
     operatorButton.addEventListener("click", () => { 
-        const operator = operatorButton.textContent; 
-        debugger; 
-        setOperator(operator)
+        setOperator(operatorButton.textContent)
     })
 })
 
 function setOperator(operator){
-    if(fullOperation.length === 0 || isNaN(fullOperation.slice(-1))){
-        return
+    if (firstOperand && !secondOperand) {
+        operation = operator;
     }
-    if(currentOperator.length === 0){
-        handleFirstOperator(operator)
-    } else if (operator === '='){
-        handleEquals();
-    } else {
-        addOperator(operator);
-    }
+    
+    
+    if (secondOperand) {
+        //handle continuous operations
+    } 
 }
-/**************************************************************************/
 
-/***********************  Button Operation Functions **********************/
-
-
-function handleFirstOperator(operator){
-    currentOperator = operator;
-    firstNumber = currentSelection;
-    fullOperation.push(operator);
-
-    updateDisplay()
-    currentSelection = '';
-}
+document.querySelector('.equals')
+.addEventListener("click", () => { 
+    handleEquals()
+})
 
 function handleEquals(){
-    updateTotal()
-    updateDisplay()
-    resetValues()
-}
-
-function addOperator(operator){
-    updateTotal()
-
-    firstNumber = total;
-    fullOperation.push(operator);
-    updateDisplay()
-
-    currentSelection = '';
-    currentOperator = operator;
-}
-/**************************************************************************/
-
-/***************************  Helper Functions ****************************/
-function updateDisplay(){
-    if(currentSelection.length > 10){
-        const converted = convertExponent()
-        document.getElementById('current-selection').textContent = converted;
-    } else {
-        document.getElementById('current-selection').textContent = currentSelection;
+    if (secondOperand) {
+        const result = calculate(); 
+        firstOperand = result; 
+        secondOperand = ''; 
+        operation = ''; 
+        setDisplay(result);
     }
-    document.getElementById('full-selection').textContent = '';
-    fullOperation.map((item) => {
-        document.getElementById('full-selection').textContent += item
-    });
 }
 
-function updateTotal(){
-    switch(currentOperator){
+function setDisplay(value) {
+    const display = document.getElementById("current-selection")
+    display.textContent = value; 
+}
+
+function calculate(){
+    switch(operation){
         case 'x':
-            total = parseFloat(firstNumber) * parseFloat(currentSelection);
-            break;
+            return `${parseFloat(firstOperand) * parseFloat(secondOperand)}`;
         case '/':
-            total = parseFloat(firstNumber) / parseFloat(currentSelection);
-            break;
+            return `${parseFloat(firstOperand) / parseFloat(secondOperand)}`;
         case '+':
-            total = parseFloat(firstNumber) + parseFloat(currentSelection);
-            break;
+            return `${parseFloat(firstOperand) + parseFloat(secondOperand)}`;
         case '-':
-            total = parseFloat(firstNumber) - parseFloat(currentSelection);
-            break;
-    }
-    if(total.toString().length > 10){
-        currentSelection = total.toFixed(2)
-    } else {
-        currentSelection = total;
+            return `${parseFloat(firstOperand) - parseFloat(secondOperand)}`;
     }
 }
 
-function convertExponent(){
-    const length = currentSelection.length - 1;
-    const num = currentSelection.charAt(0);
-    return `${num}e${length}`
-}
-
-function resetValues() {
-    currentSelection = '';
-    fullOperation = [];
-    total = 0;
-    firstNumber = 0;
-    currentOperator = '';
+function resetCalculator() {
+    firstOperand = '';
+    secondOperand = '';
+    operation = '';
+    setDisplay('0');
 }
